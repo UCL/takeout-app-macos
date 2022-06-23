906c6a1da929a200d73478362bbfa765458bd06e
David Guzman <d.guzman@ucl.ac.uk>
2022-06-23 17:02:58 +0100

    Implement buttons to read folder and spreadsheet

--- TakeoutFilter/ContentView.swift
+++ TakeoutFilter/ContentView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State
+    var spreadsheetUrl: String = ""
+    
+    @State
+    var folderUrl: String = ""
+    
     var body: some View {
         VStack {
             Text("Google Takeout Filter")
@@ -20,8 +27,14 @@ struct ContentView: View {
                     
                     Text("Folder containing Takeout files")
                     
-                    Button(action: openFolder) {
-                        Text("Select")
+                    HStack {
+                        Button(action: {
+                            let openFolderUrl = openFolder()
+                            readFolderUrl(from: openFolderUrl)
+                        }) {
+                            Text("Select")
+                        }
+                        Text("\(folderUrl)")
                     }
                     
                 }.padding(.bottom)
@@ -30,23 +43,53 @@ struct ContentView: View {
                     
                     Text("Spreadsheet with names and dates of presentation")
                     
-                    Button(action: openFolder) {
-                        Text("Select")
+                    HStack {
+                        Button(action: {
+                            let openSpreadsheetUrl = openSpreadsheet()
+                            readSpreadsheetUrl(from: openSpreadsheetUrl)
+                        }) {
+                            Text("Select")
+                        }
+                        
+                        Text("\(spreadsheetUrl)")
                     }
                     
                 }
-
+                
             }.padding()
             
         }
     }
     
-    func openFolder() {
-        // Implement
+    func openFolder() -> URL? {
+        let openPanel = NSOpenPanel()
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = true
+        openPanel.canChooseFiles = false
+        
+        let response = openPanel.runModal()
+        return response == .OK ? openPanel.url : nil
     }
     
-    func openSpreadsheet() {
-        // Implement
+    func readFolderUrl(from url: URL?) {
+        guard let url = url else { return }
+        folderUrl = url.path
+    }
+    
+    func openSpreadsheet() -> URL? {
+        let openPanel = NSOpenPanel()
+        openPanel.allowedContentTypes = [.commaSeparatedText]
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = false
+        openPanel.canChooseFiles = true
+        
+        let response = openPanel.runModal()
+        return response == .OK ? openPanel.url : nil
+    }
+    
+    func readSpreadsheetUrl(from url: URL?) {
+        guard let url = url else { return }
+        spreadsheetUrl = url.path
     }
 }
 
