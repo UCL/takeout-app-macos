6491109826e8e5022d051562930dbe96512e612b
David Guzman <d.guzman@ucl.ac.uk>
2022-06-27 13:39:52 +0100

    Rename main view

--- TakeoutFilter/Main/MainView.swift
+++ TakeoutFilter/Main/MainView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainView.swift
 //  TakeoutFilter
 //
 //  Created by David Guzman on 21/06/2022.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     
     @State
     var spreadsheetUrl: String = ""
@@ -112,6 +112,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
--- TakeoutFilter/TakeoutFilterApp.swift
+++ TakeoutFilter/TakeoutFilterApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TakeoutFilterApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
