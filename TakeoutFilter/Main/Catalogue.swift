fc758b327ed4ba6aaced1de911fedee8e6df40c0
David Guzman <d.guzman@ucl.ac.uk>
2022-06-29 13:56:18 +0100

    Implement function to retrieve entries from catalogue

--- TakeoutFilter/Main/Catalogue.swift
+++ TakeoutFilter/Main/Catalogue.swift
@@ -15,13 +15,33 @@ struct Catalogue {
         catalogueUrl = catalogue
     }
     
+    func entries() -> [Entry] {
+        var entries: [Entry] = []
+        do {
+            let stringContent = try String(contentsOfFile: catalogueUrl.path)
+            let stringLines: [String] = stringContent.components(separatedBy: "\n").filter(){$0 != ""}
+            for index in 1 ..< stringLines.count {
+                let line: [String] = stringLines[index].components(separatedBy: ",")
+                let intId = Int(line[0]) ?? 0
+                if (intId != 0) {
+                    let e: Entry = Entry(id: intId, dateOfPresentation: line[1], namesToFilter: line[2])
+                    entries.append(e)
+                }
+            }
+            return entries
+        } catch {
+            return entries
+        }
+    }
+    
     func extractIds() -> [Int] {
         var ids: [Int] = []
         do {
             let stringContent = try String(contentsOfFile: catalogueUrl.path)
-            let stringLines: [String] = stringContent.components(separatedBy: "\n")
-            for index in 1...stringLines.capacity - 1 {
-                let intId = Int(stringLines[index].components(separatedBy: ",")[0]) ?? 0
+            let stringLines: [String] = stringContent.components(separatedBy: "\n").filter(){$0 != ""}
+            for index in 1 ..< stringLines.count {
+                let line: [String] = stringLines[index].components(separatedBy: ",")
+                let intId = Int(line[0]) ?? 0
                 if (intId != 0) {
                     ids.append(intId)
                 }
@@ -30,5 +50,33 @@ struct Catalogue {
         } catch {
             return ids
         }
+    }
+    
+    struct Entry: Equatable {
+        
+        private var id: Int
+        
+        private var dateOfPresentation: String
+        
+        private var namesToFilter: String
+        
+        init(id: Int, dateOfPresentation: String, namesToFilter: String) {
+            self.id = id
+            self.dateOfPresentation = dateOfPresentation
+            self.namesToFilter = namesToFilter
+        }
+        
+        func getId() -> Int {
+            return id
+        }
+        
+        func getNamesToFilter() -> String {
+            return namesToFilter
+        }
+        
+        func getDateOfPresentation() -> String {
+            return dateOfPresentation
+        }
+
     }
 }
--- TakeoutFilterTests/CatalogueTests.swift
+++ TakeoutFilterTests/CatalogueTests.swift
@@ -18,4 +18,15 @@ class CatalogueTests: XCTestCase {
         let expected: [Int] = [1234, 5678]
         XCTAssertEqual(result, expected)
     }
+    
+    func testEntries() {
+        let bundle = Bundle(for: CatalogueTests.self)
+        let sampleUrl: URL = URL(fileURLWithPath: "CatalogueSample.csv", relativeTo: bundle.resourceURL)
+        let catalogue = Catalogue(catalogue: sampleUrl)
+        let result = catalogue.entries()
+        let e1: Catalogue.Entry = Catalogue.Entry(id: 1234, dateOfPresentation: "2022-06-28", namesToFilter: "Forename Surname")
+        let e2: Catalogue.Entry = Catalogue.Entry(id: 5678, dateOfPresentation: "2022-06-28", namesToFilter: "Forename Surname")
+        let expected: [Catalogue.Entry] = [e1, e2]
+        XCTAssertEqual(result, expected)
+    }
 }
