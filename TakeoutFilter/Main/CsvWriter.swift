1a09fc76265a7ae870a534c3bfadc0b781c47e61
David Guzman <d.guzman@ucl.ac.uk>
2022-07-07 17:54:57 +0100

    WIP Add csv headers

--- TakeoutFilter/Main/CsvWriter.swift
+++ TakeoutFilter/Main/CsvWriter.swift
@@ -9,8 +9,12 @@ import Foundation
 
 class CsvWriter {
     
+    private let aggregatesHeader: String = "Total_Number_Of_Queries,First_Query_Date"
     private let aggregatesSuffix: String = "-aggregates.csv"
+    private let queriesHeader: String = "Date,Query"
     private let queriesSuffix: String = "-queries.csv"
+    private let newline: String = "\n"
+    private let outputDir: String = "TakeoutFilter"
     
     func writeAggregates(id: Int, totalNumberQueries: Int, firstQueryDate: Date) {
         
